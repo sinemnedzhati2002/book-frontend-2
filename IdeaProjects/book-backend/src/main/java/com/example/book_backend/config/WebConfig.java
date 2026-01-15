72e49d98d516497b805cb4006ea69956d9f5fdde
sinemnedzhati-2002 <sinemnedzahti16@gmail.com>
2026-01-15 21:13:32 +0100

    Fix WebConfig

---
+++ IdeaProjects/book-backend/src/main/java/com/example/book_backend/config/WebConfig.java
@@ -0,0 +1,20 @@
+﻿package com.example.book_backend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins(
+                        "http://localhost:5173",
+                        "https://book-frontend-2-qbx4.onrender.com"
+                )
+                .allowedMethods("GET","POST","PUT","PATCH","DELETE","OPTIONS")
+                .allowedHeaders("*");
+    }
+}
